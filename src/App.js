16abe367660e5556c1506c4cc6c093fe496440f9
Nevena.Petro <a.nevena.petrovic@gmail.com>
2022-12-06 17:10:17 +0100

    context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <>Test</>;
-}
+import React, { useEffect } from 'react';
+import { ApplicationProvider } from './context';
+import './App.scss';
+
+const App = () => {
+
+  return (
+    <>
+      <ApplicationProvider>
+        <div>App</div>
+      </ApplicationProvider>
+    </>
+  );
+};
 
 export default App;
