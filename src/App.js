4c8555a92f9fc785dcdc9e8fee827996586de634
MarijaMarija064 <isailovicmarija064@gmail.com>
2022-12-06 16:57:33 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-  return <>Test</>;
-}
+import React, { useState, useEffect } from "react";
+import AppProvider from "./AppProvider";
+
+// import "./App.scss"
+
+const App = () => {
+
+  const [token, setToken] = useState("");
+
+  console.log(token);
+
+  function getToken() {
+    fetch("http://localhost:3333/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: "dev@dev.com",
+        password: "developer",
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => setToken(res.accessToken));
+  }
+
+  useEffect(() => getToken(), []);
+
+
+
+
+
+
+  return <>
+  <AppProvider token={token}/>
+  </>;
+};
 
 export default App;
