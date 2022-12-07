b456baec8327495312d9849ba421fc4b44611f91
RadosRosic <rados.rosic@gmail.com>
2022-12-07 21:58:53 +0100

    Revert "Revert "bring back nemanja's work""
    This reverts commit e09c0d0562800f96a536aac5a3a5bed9cb53fe0c.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./Search.scss"
+import React from "react";
+import "./Search.scss";
 
 const Search = () => {
   return (
     <div>
       <input type="text" placeholder="Search"></input>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
