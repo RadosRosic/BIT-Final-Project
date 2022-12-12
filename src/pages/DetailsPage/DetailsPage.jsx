c3e81d28bf100e4e23b8132dbbc559380590fb17
Nevena.Petro <a.nevena.petrovic@gmail.com>
2022-12-12 10:38:25 +0100

    css

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -33,7 +33,7 @@ const DetailsPage = () => {
         <div className="img-info-candidate glass-effect-bright">
           <div>
             <img
-              src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+              src={candidate.avatar}
               alt="candidate"
             />
           </div>
