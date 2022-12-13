4c2a4cdf5eb8853291bb976e92abb1eda4f7b621
RadosRosic <rados.rosic@gmail.com>
2022-12-14 00:06:11 +0100

    funny class name added to hb menu lol

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   const [active, setActive] = useState(false);
-  const bigMac = "active";
+  const bigMac = "bigMac";
 
   return (
     <header className="main-header">
