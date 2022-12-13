c5d789beea30dfa908e4c969f4e8c177c039cc5b
RadosRosic <rados.rosic@gmail.com>
2022-12-13 12:57:04 +0100

    improved x on modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import Button from "../Button/Button";
 import { applicationContext } from "../../context";
 import "./Header.scss";
 import { Link, useNavigate } from "react-router-dom";
